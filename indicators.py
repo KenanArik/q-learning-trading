d6c5c3b69ba963b559c669d7fc4887a013caf336
KenanArik <kenan.arik@gmail.com>
2019-04-23 23:08:08 +0200

    feat: add The Relative Strength Index (RSI) function into indicators

--- indicators.py
+++ indicators.py
@@ -20,6 +20,20 @@ def get_momentum(price, window=5):
     momentum.iloc[window:] = price.iloc[window:] / price.values[:-window] - 1
     return momentum
 
+def RSI(series, period):
+    delta = series.diff().dropna()
+    u = delta * 0
+    d = u.copy()
+    u[delta > 0] = delta[delta > 0]
+    d[delta < 0] = -delta[delta < 0]
+    u[u.index[period-1]] = np.mean( u[:period] ) #first value is sum of avg gains
+    u = u.drop(u.index[:(period-1)])
+    d[d.index[period-1]] = np.mean( d[:period] ) #first value is sum of avg losses
+    d = d.drop(d.index[:(period-1)])
+    rs = pd.stats.moments.ewma(u, com=period-1, adjust=False) / \
+    pd.stats.moments.ewma(d, com=period-1, adjust=False)
+    return 100 - 100 / (1 + rs)
+
 def get_sma_indicator(price, rolling_mean):
     """Calculate simple moving average indicator, i.e. price / rolling_mean.
 
